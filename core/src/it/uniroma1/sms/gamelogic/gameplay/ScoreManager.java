86266dc72600f255470484c62c87848880e5530c
Riccardo Petricca <PetriccaRcc@users.noreply.github.com>
2017-02-22 01:22:28 +0100

    Update ScoreManager.java

--- core/src/it/uniroma1/sms/gamelogic/gameplay/ScoreManager.java
+++ core/src/it/uniroma1/sms/gamelogic/gameplay/ScoreManager.java
@@ -1,9 +1,9 @@
 /*************************************************************************************
- * Copyright (c) 2014 - 2014, Ricciardino (Riccardo Petricca)
+ * Copyright (c) 2014 - 2017, Ricciardino (Riccardo Petricca)
  * 
  * This software is licensed under CC-BY-NC-SA license,
- * feel free to see my personal blog site at http://www.petriccarcc.com , 
- * or to fork my project at https://github.com/Ricciardino
+ * feel free to see my personal blog site at http://www.riccardopetricca.com , 
+ * or to fork my project at https://github.com/petriccarcc
  * see the LICENSE file or http://creativecommons.org/licenses/by-nc-sa/4.0/
  * for further informations and details.
  *
